4bdc5c85c8429996c93e6cfa6c82e1c4cdb8e91c
Ger Hobbelt <ger@hobbelt.com>
2024-06-27 00:45:55 +0200

    reran these 'assert failed'-prematurely terminated tests with very latest tesseract: no non-trivial change at all, so the bug/issue persists to date, QED.

--- sizes/B_RUN_data-1001-000-0003-b-leveled/tessdata/OEM3/PSM1-TH0-DSIZE-2700x1735-cmdline.sh
+++ sizes/B_RUN_data-1001-000-0003-b-leveled/tessdata/OEM3/PSM1-TH0-DSIZE-2700x1735-cmdline.sh
@@ -1,6 +1,6 @@
-if ! test -f PSM1-TH0-DSIZE-2700x1735-debug-2.log ; then
+if ! test -f ./PSM1-TH0-DSIZE-2700x1735-debug-2.log ; then
 (
-	# sometimes a tesseract run hangs; haven't found a decent clue why, so we apply a fixed timeout/abort to keep the run going, no matter what happens.
-	(  time timeout -v -k 1s 3m   "/c/Program Files/Tesseract-OCR/tesseract.exe"  --loglevel ALL -l eng --psm 1 --oem 3 --tessdata-dir ../../../../tessdata -c debug_file=PSM1-TH0-DSIZE-2700x1735-debug.log -c thresholding_method=0 -c document_title=tessdata-PSM1-OEM3-TH0-1001-000-0003-b-leveled  ../../DERIVSRC-1001-000-0003-b-leveled-DSIZE-2700x1735.webp PSM1-TH0-DSIZE-2700x1735  hocr     txt tsv  ../../../tess_run_01.conf )    > PSM1-TH0-DSIZE-2700x1735-debug-1.log   2> PSM1-TH0-DSIZE-2700x1735-debug-2.log
+    # sometimes a tesseract run hangs; haven't found a decent clue why, so we apply a fixed timeout/abort to keep the run going, no matter what happens.
+    (  time timeout -v -k 1s 3m   "/c/Program Files/Tesseract-OCR/tesseract.exe"  --loglevel ALL -l eng --psm 1 --oem 3 --tessdata-dir ../../../../tessdata -c debug_file=PSM1-TH0-DSIZE-2700x1735-debug.log -c thresholding_method=0 -c document_title=tessdata-PSM1-OEM3-TH0-1001-000-0003-b-leveled  ../../DERIVSRC-1001-000-0003-b-leveled-DSIZE-2700x1735.webp PSM1-TH0-DSIZE-2700x1735  hocr     txt tsv  ../../../tess_run_01.conf )    > ./PSM1-TH0-DSIZE-2700x1735-debug-1.log   2> ./PSM1-TH0-DSIZE-2700x1735-debug-2.log
 ) &
 fi

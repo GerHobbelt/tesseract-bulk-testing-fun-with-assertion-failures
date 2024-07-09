9ec9fa1f212dc6dbeda16571b374a44625fa66dc
Ger Hobbelt <ger@hobbelt.com>
2024-07-09 21:53:53 +0200

    tweaked the config files some more + fix for the weird-segfault-hunter.sh script: truly wait for all tesseract instances to have finished before re-running them.

--- sizes/weird-segfault-hunter.sh
+++ sizes/weird-segfault-hunter.sh
@@ -5,6 +5,14 @@
 ROUND=0
 
 rm ./weird-segfault-hunted/segfaults-found.grep.lst
+rm ./weird-segfault-hunted/*.log
+rm ./weird-segfault-hunted/*.webp
+rm ./weird-segfault-hunted/*.html
+rm ./weird-segfault-hunted/*.hocr
+rm ./weird-segfault-hunted/*.tsv
+rm ./weird-segfault-hunted/*.txt
+rm ./weird-segfault-hunted/*.unlv
+
 
 for f in * ; do 
 	((ROUND=ROUND+1))
@@ -14,6 +22,11 @@ for f in * ; do
 	done
 	
 	echo "Waiting for the last tesseract runs to finish..."
+
+	# wait a little while as otherwise `ps ax` won't see the last tesseract command that was pushed into the background above.
+	# while this observation delay doesn't really matter all that much in the loop above, it does now!
+	sleep 0.25
+
 	while true ; do
 		if test $( ps ax | grep -e tesseract | wc -l ) -eq 0 ; then
 			break
@@ -21,5 +34,6 @@ for f in * ; do
 		echo "sleep..."
 		sleep 1
 	done
+	# exit 1
 done
 echo "Total rounds of hunting: #${ROUND}"

53ecae31a536c6fc3fcddb0cecfb0b242f744662
Ger Hobbelt <ger@hobbelt.com>
2024-07-09 16:49:33 +0200

    fix for windows `ps ax` observation delay:
    # wait a little while as otherwise `ps ax` won't see the last tesseract command that was pushed into the background above.
    # while this observation delay doesn't really matter all that much in the loop above, it does now!
    sleep 0.25

--- sizes/run_all_HTML_debug_diag_demo_output.sh
+++ sizes/run_all_HTML_debug_diag_demo_output.sh
@@ -55,6 +55,10 @@ done
 
 # wait for the last dregs to completely finish, before we call it The End...
 
+# wait a little while as otherwise `ps ax` won't see the last tesseract command that was pushed into the background above.
+# while this observation delay doesn't really matter all that much in the loop above, it does now!
+sleep 0.25
+
 echo "Waiting for the last tesseract runs to finish..."
 while true ; do
     if test $( ps ax | grep -e tesseract | wc -l ) -eq 0 ; then

d18d2a0d99bd6f4e9f25a813a2180edff81e7afb
Ger Hobbelt <ger@hobbelt.com>
2024-07-20 19:58:20 +0200

    enhance the discover-failed-tesseract-runs.sh script to produce lists of test runs which apparently failed fatally and deserve closer inspection about what went wrong so horribly.

--- sizes/discover-failed-tesseract-runs.sh
+++ sizes/discover-failed-tesseract-runs.sh
@@ -5,4 +5,24 @@
 
 # find -type f -name '*debug-2.log' | xargs -n 100 grep -i -e 'assert failed' -l | xargs -n 10 rm
 
-find -type f -name '*-debug-2.log' | xargs -n 10 grep -i -e 'assert failed' -l | sed -E -e 's/^.*RUN_data-//' -e 's/-debug-2.log//' > failed-tesseract-runs.list
+find -type f -name '*-debug-2.log' | xargs -n 10 grep -i -e 'assert failed' -l | sed -E -e 's/^.*RUN_data-//' -e 's/-debug-2.log//' > failed-tesseract-runs.list.tmp
+find -type f -name '*-debug-2.log' | xargs -n 10 grep -e 'Error during processing' -l  | sed -E -e 's/^.*RUN_data-//' -e 's/-debug-2.log//' >> failed-tesseract-runs.list.tmp
+# 'Leptonica Error in'...
+
+cat failed-tesseract-runs.list.tmp | sort | uniq > failed-tesseract-runs.list
+
+# these reported errors, not necessarily fatal failures:
+find -type f -name '*-debug-2.log' | xargs -n 10 grep -i -e 'ERROR' -l  | sed -E -e 's/^.*RUN_data-//' -e 's/-debug-2.log//' >> errored-tesseract-runs.list
+
+# it is a pretty sure indication that something went horribly wrong when the intermediate images have not been produced:
+#
+# for vanilla, that is the XYZ-processed.tif, for ourselves that's the HTML file plus a multitude of webp files.
+
+rm misc-fatalities-tesseract-runs.list
+for d in vanilla-output A_RUN_data-* A_RUN_data-* ; do
+	for f in $( find $d   -type f -name '*-debug-2.log' | sed -E -e 's/-debug-2.log//' ) ; do
+		if ! test -f $f*.processed.tif || ! test -f $f*.processed.tif ; then
+			echo $f >>  misc-fatalities-tesseract-runs.list
+		fi
+	done
+done
